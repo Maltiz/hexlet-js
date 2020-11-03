deab65bf3150a4f238692a93fce26529f89e6d5f
maltiz <hastalavista995@gmail.com>
2020-11-03 21:29:08 +0300

    add sloc, hexo.io, prettier and .gitignore

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import _ from "lodash";
 
 console.log("Hello, World");
-console.log(_.last(['one', 'two']));
+console.log(_.last(["one", "two"]));
+const first = 3;
+const second = 6;
+const sum = (first, second) => {
+  let result = first + second;
+  return result;
+};
+console.log(sum(first, second));
